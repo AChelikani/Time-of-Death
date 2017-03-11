92ae308bec6aaa4337e8adacdca799546c46d70c
Advith Chelikani <advith.chelikani@gmail.com>
2017-03-10 19:33:53 -0800

    update visual and algorithm

--- cubeView.py
+++ cubeView.py
@@ -1,26 +1,27 @@
 from Tkinter import *
-import takePicture
+#import takePicture
 import deathPrediction
+import time
 
 class CubeView(object):
     def __init__(self):
         self.root = Tk()
-        self.camera = takePicture.Photograph()
+        #self.camera = takePicture.Photograph()
         self.engine = deathPrediction.DeathPredictor()
         self.root.wm_attributes("-fullscreen", True)
         self.dateVar = StringVar()
-        self.label = Label(self.root, textvariable=self.dateVar, font=("Helvetica", 100), pady=100)
+        self.label = Label(self.root, textvariable=self.dateVar, font=("Helvetica", 100), bg="black", fg="white", pady=100)
         self.root.bind('<Left>', self.run)
-        self.root.bind('<Right>', self.refresh)
+        self.root.bind('<Up>', self.photograph)
+        self.root.configure(background="black")
         self.root.mainloop()
 
     def run(self, event):
-        self.dateVar.set("Hey")
-        self.label.pack()
-
-    def refresh(self, event):
-        self.dateVar.set("Yo")
-        self.label.pack()
+        self.labelTest()
+        #time.sleep(5)
+        #self.setPhase2()
+        #time.sleep(5)
+        #self.setPhase3()
 
     def createLabel(self, prediction):
         lab = prediction["month"] + " " + prediction["day"] + ", " + prediction["year"]
@@ -30,8 +31,21 @@ class CubeView(object):
     def photograph(self, event):
         self.camera.takePicture()
         age = self.engine.agePredict("image.jpg")
-        prediction = self.engine.deathPredictNaive(age)
+        prediction = self.engine.deathPredict(age)
         self.createLabel(prediction)
+
+    def labelTest(self):
+        age = self.engine.agePredict("face1.jpg")
+        prediction = self.engine.deathPredict(age)
+        self.createLabel(prediction)
+
+    def setPhase2(self):
+        self.dateVar.set("Spend every moment of your life deliberately.")
+        self.label.pack()
+
+    def setPhase3(self):
+        self.dateVar.set("You're still here. Is it because you want to be?")
+        self.label.pack()
 
 
 if __name__ == "__main__":
--- deathPrediction.py
+++ deathPrediction.py
@@ -1,13 +1,14 @@
 import angus
 from pprint import pprint
 import constants
-
+import random
 
 
 class DeathPredictor(object):
     def __init__(self):
         conn = angus.connect()
         self.service = conn.services.get_service('age_and_gender_estimation', version=1)
+        self.ageDist = self.parseAgeDict("ageDist.txt")
 
     # Predict the age of the person in a given picture
     def agePredict(self, imgPath):
@@ -23,8 +24,28 @@ class DeathPredictor(object):
         prediction["day"] = str(constants.TODAY_DAY)
         return prediction
 
+    def parseAgeDict(self, fname):
+        d = {}
+        age = 1
+        with open(fname, 'r') as f:
+            for line in f:
+                d[age] = float(line.rstrip())
+                age += 1
+        return d
+
+    def deathPredict(self, age):
+        prediction = {}
+        if int(age) == age:
+            yearsUntilDeath = int(self.ageDist[age])
+        else:
+            yearsUntilDeath = int(self.ageDist[int(age)] + (self.ageDist[int(age+1)]-self.ageDist[int(age)])*(age - int(age)))
+        prediction["year"] = str(constants.TODAY_YEAR + yearsUntilDeath)
+        prediction["month"] = constants.MONTHS[random.randint(0,11)]
+        prediction["day"] = str((constants.TODAY_DAY + random.randint(0,31))%27 + 1)
+        return prediction
+
 
 if __name__ == "__main__":
     dp = DeathPredictor()
     age = dp.agePredict("face1.jpg")
-    print dp.deathPredictNaive(age)
+    print dp.deathPredict(age)
